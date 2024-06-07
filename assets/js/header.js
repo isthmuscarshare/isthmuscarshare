86abf9d428a45be9b67e88885cdd456fa280b951
will.ochowicz <will.ochowicz@genusplc.com>
2024-06-07 09:30:17 -0500

    Hide/show "Manage Reservations" menu when logged out/in

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,16 +1,19 @@
-import { updateName, signOut } from "./authentication.js"
-updateName(name => {
+import { updateName as loginStatusChange, signOut } from "./authentication.js"
+loginStatusChange(name => {
     const loginLink = document.getElementById("liLoginLink")
     const liDisplayUserName = document.getElementById("liDisplayUserName")
     const pUserName = document.getElementById("pDisplayUserName")
+    const liManageReservation = document.getElementById("liManageReservation")
     if(name != null){
         loginLink.style.display = "none"
         liDisplayUserName.style.display = null
         pUserName.innerHTML = name
+        liManageReservation.style.display = null
     }
     else{
         loginLink.style.display = null
         liDisplayUserName.style.display = "none"
+        liManageReservation.style.display = "none"
     }
 })
 const logoutLink = document.getElementById("aLogoutLink")
